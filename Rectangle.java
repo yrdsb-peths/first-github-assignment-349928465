50733e4c397ae6e216029c044174d469749583b2
349928465 <130993268+349928465@users.noreply.github.com>
2023-04-19 08:35:02 -0400

    first version of Rectangle.java file

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+public class Rectangle
+{
+    private int width;
+    private int height;
+    
+    public Rectangle(int rectWidth, int rectHeight)
+    {
+        width = rectWidth;
+        height = rectHeight;
+    }
+}
