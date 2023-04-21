582aefac163d9c9060f6ca2748340f4967b01af1
349928465 <349928465@gapps.yrdsb.ca>
2023-04-21 08:47:29 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,10 +2,12 @@ public class Student
 {
 	private int age;
 	private String grade;
+	private int volunteerHours;
 
-	public Student(int theAge, String theGrade)
+	public Student(int theAge, String theGrade, int theVolunteerHours)
 	{
 		age = theAge;
 		grade = theGrade;
+		volunteerHours = theVolunteerHours;
 	}
 }
