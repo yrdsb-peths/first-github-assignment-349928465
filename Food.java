6b93d6dbc32504326b0e195e4c057d76d6793274
349928465 <349928465@gapps.yrdsb.ca>
2023-04-26 08:34:57 -0400

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,6 +1,13 @@
-public Class Food
+public class Food
 {
 	private int grams;
 	private String name;
 	private boolean hot;
+	
+	public Food(int grams, String name, boolean hot)
+	{
+		this.grams = grams;
+		this.name = name;
+		this.hot = hot;
+	}
 }
